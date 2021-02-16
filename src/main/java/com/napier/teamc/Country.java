071c4419749fcf97a23bf7807e147fe2068e44e5
Jackson20010424 <40456371@live.napier.ac.uk>
2021-02-16 17:39:25 +0000

    Creating a country class with only 2 properties. First - population & name.

---
+++ src/main/java/com/napier/teamc/Country.java
@@ -0,0 +1,19 @@
+package com.napier.teamc;
+/**
+ * Represents a Country
+ */
+public class Country
+{
+    // Country population
+    public int population;
+
+    // Country name
+    public String name;
+
+    // Write output
+    public String toString()
+    {
+        return "name " + this.name + " population " + this.population;
+    }
+}
+
