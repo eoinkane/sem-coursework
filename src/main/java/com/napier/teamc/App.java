17e2645d74dc2fc89cbcba6b39c38fa1ffc45c9f
BoeJell2001 <40509170@live.napier.ac.uk>
2021-02-27 23:07:31 +0000

    Making city class and adding ticket #26 to app.

--- src/main/java/com/napier/teamc/App.java
+++ src/main/java/com/napier/teamc/App.java
@@ -44,7 +44,7 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://localhost:3306/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://db:3306/world?useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
