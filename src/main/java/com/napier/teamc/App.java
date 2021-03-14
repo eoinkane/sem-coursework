cd19a90a28fee52b7e833f5c4b73aa58446d69d7
eoinkane <eoinkane23@gmail.com>
2021-03-14 18:57:53 +0000

    Allow Public Key Retrieval

--- src/main/java/com/napier/teamc/App.java
+++ src/main/java/com/napier/teamc/App.java
@@ -46,7 +46,7 @@ public class App
                 // Wait a bit for db to start
                 Thread.sleep(30000);
                 // Connect to database
-                con = DriverManager.getConnection("jdbc:mysql://" + location + "/world?useSSL=false", "root", "example");
+                con = DriverManager.getConnection("jdbc:mysql://" + location + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
                 break;
             }
