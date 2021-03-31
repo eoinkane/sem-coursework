c4f5342bc6f07c5b3f1c9843dc106419756b9806
eoinkane <eoinkane23@gmail.com>
2021-04-01 00:43:49 +0100

    #21 Refactor to outsource query execution

--- src/main/java/com/napier/teamc/App.java
+++ src/main/java/com/napier/teamc/App.java
@@ -1008,21 +1008,28 @@ public class App
      */
     public ArrayList<City> getTopNPopulatedCitiesinaCountry(int n)
     {
+        // Handle invalid input
+        if (n < 0) return null;
+
+        // method initialisation
+        String strSelect =
+                "WITH grouped_cities AS (SELECT city.Name AS city_name, c.name AS Name, city.Population, ROW_NUMBER() OVER "
+                        + "(PARTITION BY Name ORDER BY Population DESC) row_num FROM city "
+                        + "JOIN country c ON city.CountryCode = c.Code) "
+                        + "SELECT city_name, Name, Population FROM grouped_cities WHERE row_num <= <N>;";
+        String errorMessage = "Failed to get the top N populated cities in a country.";
+
+        strSelect = strSelect.replace("<N>", String.valueOf(n));
+
+        // Execute query on the connected database, and if something goes wrong print the given error message
+        ResultSet rset = query(strSelect, errorMessage);
+
+        // While dealing with the result set, catch any SQLException that can be thrown
         try
         {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "WITH grouped_cities AS (SELECT city.Name AS city_name, c.name AS Name, city.Population, ROW_NUMBER() OVER "
-                            + "(PARTITION BY Name ORDER BY Population DESC) row_num FROM city "
-                            + "JOIN country c ON city.CountryCode = c.Code) "
-                            + "SELECT city_name, Name, Population FROM grouped_cities WHERE row_num <= <N>;";
-            strSelect = strSelect.replace("<N>", String.valueOf(n));
-            // Execute SQL
-            ResultSet rset = stmt.executeQuery(strSelect);
-            // Extract country information.
+            // Extract city information from query results.
             ArrayList<City> cities = new ArrayList<City>();
+            // Create a new city object for each record in the result set and add the object to the array
             while (rset.next()) {
                 City cty = new City(
                         rset.getString("city_name"),
@@ -1035,13 +1042,15 @@ public class App
                 );
                 cities.add(cty);
             }
-            // return results
+            // Return the results of the method.
             return cities;
         }
+        // If an error occurs while handling the result set then don't crash the application,
+        // instead return null and print an error message
         catch (SQLException e)
         {
             System.out.println(e.getMessage());
-            System.out.println("Failed to get the top N populated cities in a country.");
+            System.out.println(errorMessage);
             return null;
         }
     }
