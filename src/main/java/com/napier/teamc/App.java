8ffbc0e9da7ea76d067cefc1d9e182154c64c0d0
eoinkane <eoinkane23@gmail.com>
2021-04-01 01:08:13 +0100

    #8 Refactor to outsource query execution

--- src/main/java/com/napier/teamc/App.java
+++ src/main/java/com/napier/teamc/App.java
@@ -1340,20 +1340,20 @@ public class App
     }
 
     public ArrayList<String> getPopulatedAndUnpopulatedCitiesForRegion() {
-
+        // method initialisation
+        String strSelect =
+                "SELECT in_regions.region AS 'region_name', in_regions.region_population, in_cities.population_in_cities, (in_regions.region_population - in_cities.population_in_cities) AS 'population_not_in_cities' FROM (SELECT Region AS 'region' , SUM(Population) AS 'region_population' \n" +
+                        "FROM country GROUP BY Region) in_regions "
+                        +"LEFT JOIN (SELECT country.Region AS 'region', SUM(city.Population) AS 'population_in_cities' "
+                        +"FROM city JOIN country ON city.CountryCode = country.Code GROUP BY Region) in_cities ON in_cities.region = in_regions.region;";
+        String errorMessage = "Failed to get the population information in and out of cities.";
         String x = null;
 
+        // Execute query on the connected database, and if something goes wrong print the given error message
+        ResultSet rset = query(strSelect, errorMessage);
+
+        // While dealing with the result set, catch any SQLException that can be thrown
         try {
-            // Create an SQL statement
-            Statement stmt = con.createStatement();
-            // Create string for SQL statement
-            String strSelect =
-                    "SELECT in_regions.region AS 'region_name', in_regions.region_population, in_cities.population_in_cities, (in_regions.region_population - in_cities.population_in_cities) AS 'population_not_in_cities' FROM (SELECT Region AS 'region' , SUM(Population) AS 'region_population' \n" +
-                            "FROM country GROUP BY Region) in_regions "
-                            +"LEFT JOIN (SELECT country.Region AS 'region', SUM(city.Population) AS 'population_in_cities' "
-                            +"FROM city JOIN country ON city.CountryCode = country.Code GROUP BY Region) in_cities ON in_cities.region = in_regions.region;";
-            // Execute SQL statement
-            ResultSet rset = stmt.executeQuery(strSelect);
             ArrayList<String> ContinentInfo = new ArrayList<String>();
             // Display
             while (rset.next()) {
